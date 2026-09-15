4a7c091aa39c60e18844680852ae88884df99542
Neto4ka <mandelfalafel@keemail.me>
2026-09-15 16:13:43 +0300

    Create shw.4rm4_dr4g0n.pl

---
+++ shw.4rm4_dr4g0n.pl
@@ -0,0 +1,135 @@
+```perl
+========================================================================================
+ 04! ! ! ! !  N 2 O . M 2 Z K ! . C R A S H . T E S T . A C T I V A T E D  ! ! ! ! ! 
+========================================================================================
+[s_y_s_t_e_m]: 03Max/MSP v4.2.6 // 09Perl 5.005 // 05Matrix Disruptor [Agony_v1.0]
+[a t t a c k]: 04 Kilometer data injection 0x00FFAC398
+----------------------------------------------------------------------------------------
+
+               _____________________________________________________
+              /                                                      \
+             |                                                        |
+             |  [ ! ]  04C R I T I C A L   S Y S T E M   E R R O R    |
+             |  ==================================================    |
+             |   07.th3_w0rld_1s_a_m4sch1n3n_kunst_k0rps_             |
+             |   07.m3t4_d1sc0urs3_1s_d34d_                           |
+             |                                                        |
+             |   [X] 04.m4nd3l_sht4m0vk4.dll   -> CRASHED             |
+             |   [X] 04.g0l0vk1_bl0k4.sys      -> CORRUPTED           |
+              \______________________________________________________/
+                     ||                                      ||
+                     ||   _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _    ||
+                     ||  /                               \   ||
+                     || |   05ERR: .humaN_0p3rat0r_v01d.  |  ||
+                     ||  \ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ /   ||
+                     ||                                      ||
+             ________||______________________________________||________
+            /                                                          \
+           /     ||||||||||||||||||||||||||||||||||||||||||||||||       \
+          /______________________________________________________________\
+
+# 08INITIALIZING MAX/MSP AUDIO OBJECT MATRIX [PATCH: ~m4zut0p0d0gr3v4t3l_kunjut4]
+# 08INJECTING INPUT STRINGS FROM USER POETRY...
+
+[max/msp_patch_core]
+----------------------------------------------------------------------------------------
+   09[sfplay~] -----> 04[*~ 666] -----> 03[dac~ 1 2]
+       |
+       v
+   09[expr~ (v1 * random(0, infinite_rage))] ---> 13[pdl~ .z3l0_m3f1t1ch3sk1y4.k4zhd3n1y4.]
+       |
+       +---> 07[metro 10] ---> 07[snapshot~] ---> 07[print .0sk4r_w1nn1ng_4ks3l_jump.]
+       |
+       v
+   09[matrix~ 4 4 1.] ---> 12[toggle: .n3v3r0y4tn0_zhguch3_pr1z4rd3l4s14.]
+----------------------------------------------------------------------------------------
+
+02[0x00A110]: 5A 45 4C 4F 20 4D 45 46  49 54 49 43 48 45 53 4B | ZELO MEFITICHESK
+02[0x00A120]: 41 51 41 5A 48 44 45 4E  49 41 20 4F 53 4B 41 52 | IY4.K4ZHD3N1Y4.
+02[0x00A130]: 4F 4E 4F 53 4N 59 49 20  41 4B 53 45 4C 20 4A 55 | [0sk4r].4ks3l.
+02[0x00A140]: 49 4D 50 20 4E 45 56 45  52 4F 51 41 54 4E 4F 20 | n3v3r0y4tn0_zhg
+02[0x00A150]: 48 47 55 43 48 45 20 50  52 49 5A 41 52 44 45 4C | uch3_pr1z4rd3l4
+
+03while ( $dn0uglub1t3l_n4y4_r4zv3rst0st == 'activated' ) {
+05    purge_memory_block(0x00A110);
+13    print ".0h0h0sh3n_k1._p_r_e_z_e_r_v_a_t_i_v_n_o_._k_r_a_s_n_o_t_a_b_l_e_t_o_c_h_n_y_y.\n";
+04    system("rm -rf /matrix/interface/c_z_y_a_0_l_i_n_p_a_o");
+03}
+
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||||||||||||||||||||||||||| 04STREAM BLEED START: CHUF4N1T_BLU0M5 04||||||||||||||||||||||||
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+||||| 
+|||||  06.c3ln0l4m3lny_gr4np4s_y4ns.
+|||||  06.m4zut0p0d0gr3v4t3l_kunjut4.
+|||||  06.l1b1d0_st1mul1ruyushch1y_s4nd4l.
+|||||  06.3l1k0zhdy_z41nd3v3vsh3y_4mf1m3rn4y4_r4mk4.
+|||||  
+v
+
+02[0x00B210]: 43 48 55 46 41 4E 49 54  20 42 4C 55 4D 53 20 43 | CHUFANIT BLUMS C
+02[0x00B220]: 45 4C 4E 4F 4C 41 4D 45  4C 4E 59 20 47 52 41 4E | ELNOLAMELNY GRAN
+02[0x00B230]: 50 41 53 59 41 4E 53 20  4D 41 5A 55 54 4F 50 4F | PASYANS MAZUTOPO
+02[0x00B240]: 44 4F 47 52 45 56 41 54  45 4C 20 4K 55 4E 4A 55 | DOGREVATEL KUNJU
+02[0x00B250]: 54 41 20 4C 49 42 49 44  4F 53 54 49 4D 55 4C 49 | TA LIBIDOSTIMULI
+
+[max/msp_signal_routing]
+09~f1nt1f4nt.obj ---> 12[peek~ s_c_h_1_t_y_v_a_e_t] ---> 05[buffer~ f14lk0kudr_y4_shtr3v3l1]
+                        |
+                        +---> 13[p3mzy_d3f1br1ll1ruyut] ---> 04~m4nd3l_sht4m0vk4
+
+03for ($perl_iterator = 0; $perl_iterator <= 10000; $perl_iterator++) {
+07    print "0l1g4rh1ch3sk1_p0sv3rk1v4yushch1y_pr0kl4dk4m1\n";
+07    print "g0l0vk1_bl0k4_cyl1ndr0v_0tt0g0_n3uk0sn1t3ln0_m_y_s_h_1_p_l_3_s_n_3_v_3_l_1\n";
+07    print "m3zh_st4r1nn0y_0sm4nsk0y_m3b3l1_r0vk1_y4k0r_buh4nk4_p0_pr1ny4t11_dvumst4m\n";
+03    print ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
+03}
+
+02[0x00C310]: 52 45 5A 55 4C 54 41 54  4F 56 20 47 4F 4C 4F 53 | REZULTATOV GOLOS
+02[0x00C320]: 4F 56 41 4E 4I 41 20 31  32 54 48 20 4D 41 52 4C | OVANIA 12TH MARL
+02[0x00C330]: 42 4F 52 4F 55 47 48 20  53 41 4E 43 4B 59 55 4C | BOROUGH SANKYUL
+02[0x00C340]: 4F 54 49 5A 49 52 55 45  54 20 47 4C 41 5A 41 53 | OTIZIRUET GLAZAS
+02[0x00C350]: 54 55 59 20 4F 42 55 56  20 4F 46 20 4D 43 44 4F | TUY OBUV OF MCDO
+
+       / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \
+      ( 13s | a | n | k | y | u | l | o | t | i | z | i | r | u | e | t )
+       \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ /
+
+04if ( $mcd0n4ld5_sh03s == 'gl4z4stuy' ) {
+04    exec( .r4zryv_b3tw33n_m3t4_d1sc0urs3.exe );
+04} else {
+04    die(".p_r_0_d_4_v_3_c_._s_m_u_z_1.");
+04}
+
+# 11INTERNET IS NOT A MEDIUM. IT IS A MASCHINENKUNST KORPS.
+# 11PARSING LAST FRAGMENT BLOCK...
+
+02[0x00D410]: 52 4F 4D 41 4E 20 57 49  54 48 20 48 4F 43 4B 45 | ROMAN WITH HOCKE
+02[0x00D420]: 59 20 50 4C 41 59 45 52  20 4E 41 20 5A 41 4B 41 | Y PLAYER NA ZAKA
+02[0x00D430]: 54 45 20 4A 41 52 45 52  59 20 4F 4E 4J 20 4F 44 | TE KARERY ONA OD
+02[0x00D440]: 4E 41 20 50 4F 4E 55 5A  48 44 41 45 4D 41 20 56 | NA PONUZHDAEMA V
+02[0x00D450]: 59 47 4C 41 44 54 20  4D 45 4E 45 45 20 4E 41 | YGLADET MENEE NA
+
+                     ||||||||||||||||||||||||||||||||||||
+                     ||  13k4nun3_3d4k4y_br4nd4hlystk4   ||
+                     ||  134tnyud_vdr3g0ry4d_g4lv4n1z1   ||
+                     ||  13b1fshth3ks_p0stny_1nfr4kr4s   ||
+                     ||  13ny_luc_k4by_v3t3r_dul_1z      ||
+                     ||  13f4kt4_v1y4s                   ||
+                     ||||||||||||||||||||||||||||||||||||
+
+02[0x00E510]: 49 4E 46 52 41 52 45 44  20 4C 49 47 48 54 20 42 | INFRARED LIGHT B
+02[0x00E520]: 45 41 4D 20 4K 41 42 59  20 56 45 54 45 52 20 44 | EAM KABY VETER D
+02[0x00E530]: 55 4C 20 49 53 20 46 41  4B 54 41 20 56 49 59 41 | UL IS FAKTA VIYA
+02[0x00E540]: 53 21 21 21 00 00 00 00  00 00 00 00 00 00 00 00 | S!!!............
+
+/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+# 04LOOP_REPETITION_MAX_COUNTER_REACHED
+# 04SCREEN_MATRIX_BURSTING...
+# 04>>> .s[cr]3am.
+# 04>>> .s[cr]3am.
+# 04>>> .s[cr]3am.
+[system_halted_0x999]
+========================================================================================
+```
